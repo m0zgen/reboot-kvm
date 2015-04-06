2b7cf34efe58963367d1ee6258f3233c9a823c78
m0zgen <g.yevgeniy.p@gmail.com>
2015-04-06 12:10:39 +0600

    add uninstall params

--- install.sh
+++ install.sh
@@ -8,12 +8,35 @@ VAR=/usr/local/sbin
 RUN=/usr/bin/reboot-kvm
 SCR=/usr/local/sbin/reboot-kvm.sh
 
-curl http://xxx.xxx.xxx.x/scripts/reboot-kvm/reboot-kvm.sh > $SCR
-chmod +x $SCR
+install(){
+	curl http://ala01-web03.rb.kz/scripts/reboot-kvm/reboot-kvm.sh > $SCR
+	chmod +x $SCR
 
 
-echo -e "export S_PATH=$VAR\n\$S_PATH/reboot-kvm.sh $*" > $RUN
-chmod +x $RUN
+	echo -e "export S_PATH=$VAR\n\$S_PATH/reboot-kvm.sh \$*" > $RUN
+	chmod +x $RUN
 
-echo -e "Install done!"
+	echo -e "Install done!"
+}
+
+uninstall(){
+	/usr/bin/rm $RUN
+	/usr/bin/rm $SCR
+
+	echo -e "Uninstall done!"
+}
+
+if [ $# -eq 0 ]; then
+    install
+    exit 1
+fi
+
+for i in "$@" ; do
+
+    if [[ $i == "--uninstall" ]] ; then
+    	uninstall
+        break
+    fi
+
+done
 
